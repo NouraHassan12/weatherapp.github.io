67c916fa3222e49b2272dea016c20bf19ac72d38
Noura Hassan <54744761+NouraHassan12@users.noreply.github.com>
2020-07-05 17:12:15 -0700

    Updates

---
+++ precache-manifest.5739170cae703323e977c7343826217f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df35c7d57d5bf062f29966a0bb233eb9",
+    "url": "/weatherapp.github.io/index.html"
+  },
+  {
+    "revision": "9d2d40eac3f2bd998f30",
+    "url": "/weatherapp.github.io/static/css/main.008db962.chunk.css"
+  },
+  {
+    "revision": "33fc031029fa63168417",
+    "url": "/weatherapp.github.io/static/js/2.15f9b930.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weatherapp.github.io/static/js/2.15f9b930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d2d40eac3f2bd998f30",
+    "url": "/weatherapp.github.io/static/js/main.ac454b26.chunk.js"
+  },
+  {
+    "revision": "592a91b7a04144dd00cb",
+    "url": "/weatherapp.github.io/static/js/runtime-main.9ea96164.js"
+  },
+  {
+    "revision": "fe6634e90c5d62a96dc30021665af185",
+    "url": "/weatherapp.github.io/static/media/Homeimg.fe6634e9.png"
+  }
+]);
